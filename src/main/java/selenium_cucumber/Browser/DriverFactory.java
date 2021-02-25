7ed08648ea0c55b48fbae8bfa354e888221dac6c
orpheustaken <matheusvinicius7000@gmail.com>
2021-02-25 12:31:39 -0300

    Linux and Windows support

--- src/main/java/selenium_cucumber/Browser/DriverFactory.java
+++ src/main/java/selenium_cucumber/Browser/DriverFactory.java
@@ -8,15 +8,41 @@ public class DriverFactory {
 
     private static WebDriver driver;
     private DriverFactory() {}
+
+    public static final class osUtils {
+        private static String OS = null;
+        public static String getOsName() {
+            if(OS == null) {
+                OS = System.getProperty("os.name");
+            }
+            return OS;
+        }
+        public static boolean isWindows() {
+            return getOsName().startsWith("Windows");
+        }
+        public static boolean isLinux() {
+            return getOsName().startsWith("Linux");
+        }
+    }
     
     public static WebDriver getDriver() {
         String projectPath = System.getProperty("user.dir");
         if(driver == null) {
-            switch (Properties.browser) {
-                case FIREFOX: System.setProperty("webdriver.gecko.driver", projectPath + "/src/main/java/selenium_cucumber/Browser/Drivers/geckodriver.exe");
-                driver = new FirefoxDriver();break;
-                case CHROME: System.setProperty("webdriver.chrome.driver", projectPath + "/src/main/java/selenium_cucumber/Browser/Drivers/chromedriver.exe");
-                driver = new ChromeDriver();break;
+            if(osUtils.isWindows()) {
+                switch (Properties.browser) {
+                    case FIREFOX: System.setProperty("webdriver.gecko.driver", projectPath + "/src/main/java/selenium_cucumber/Browser/Drivers/geckodriver.exe");
+                    driver = new FirefoxDriver();break;
+                    case CHROME: System.setProperty("webdriver.chrome.driver", projectPath + "/src/main/java/selenium_cucumber/Browser/Drivers/chromedriver.exe");
+                    driver = new ChromeDriver();break;
+                }
+            }
+            if(osUtils.isLinux()) {
+                switch (Properties.browser) {
+                    case FIREFOX: System.setProperty("webdriver.gecko.driver", projectPath + "/src/main/java/selenium_cucumber/Browser/Drivers/geckodriver");
+                    driver = new FirefoxDriver();break;
+                    case CHROME: System.setProperty("webdriver.chrome.driver", projectPath + "/src/main/java/selenium_cucumber/Browser/Drivers/chromedriver");
+                    driver = new ChromeDriver();break;
+                }
             }
          }
          driver.manage().window().maximize();
